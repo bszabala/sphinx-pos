72b4150cbe276c4d3d0071bb87a3039a5a0bbd64
Bazz Zabala <bazzzabala77@gmail.com>
2023-07-28 11:13:07 +0800

    added dashboard template

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,28 +11,29 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function sendError($data = [], $http_code_response = 404){
+    public function sendError($data = [], $http_code_response = 404)
+    {
         return response()->json([
             'success' => false,
             'message' => $data,
-            'data'  => []
+            'data'    => []
         ], $http_code_response);
     }
 
     public function sendSuccess($data = [], $http_code_response = 200)
     {
-        if(getType($data) == 'string'){
+        if (getType($data) == 'string') {
             return response()->json([
-                'success'   => true,
-                'message'   => $data,
-                'data'      => $data
-            ] ,$http_code_response);
+                'success' => true,
+                'message' => $data,
+                'data'    => $data
+            ], $http_code_response);
         }
 
         return response()->json([
-            'success'   => true,
-            'message'    => "Success",
-            'data'      => $data,
-        ] ,$http_code_response);
+            'success' => true,
+            'message' => "Success",
+            'data'    => $data
+        ], $http_code_response);
     }
 }
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -5,11 +5,11 @@
             <div class="col-12 mt-5 xl:mt-0 text-center">
                 <img :src="'images/logo-' + logoColor + '.svg'" alt="Sakai logo" class="mb-5" style="width:81px; height:60px;">
             </div>
-            <div class="col-12 xl:col-6" style="border-radius:56px; padding:0.3rem; background: linear-gradient(180deg, var(--primary-color), rgba(33, 150, 243, 0) 30%);">
+            <div class="col-12 xl:col-4" style="border-radius:56px; padding:0.3rem; background: linear-gradient(180deg, var(--primary-color), rgba(33, 150, 243, 0) 30%);">
                 <div class="h-full w-full m-0 py-7 px-4" style="border-radius:53px; background: linear-gradient(180deg, var(--surface-50) 38.9%, var(--surface-0));">
                     <div class="text-center mb-5">
-                        <img src="images/avatar.png" alt="Image" height="50" class="mb-3">
-                        <div class="text-900 text-3xl font-medium mb-3">Welcome, Isabel!</div>
+                        <!-- <img src="images/avatar.png" alt="Image" height="50" class="mb-3"> -->
+                        <!-- <div class="text-900 text-3xl font-medium mb-3">Welcome, back!</div> -->
                         <span class="text-600 font-medium">Sign in to continue</span>
                     </div>
                     <transition-group name="p-message" tag="div" v-if="form.errors">
